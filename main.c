aad4ffdb372033a5a460d8b00cc707951a42e240
kvasa <kvasan2104@gmail.com>
2023-04-13 19:37:29 +0300

    testing...

--- main.c
+++ main.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 
+int line_counter(char *buffer, int len) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        if (buffer[i] == '\n')
+            count++;
+    }
+    return count;
+}
+
+int int_counter(char *line, int len) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        if (line[i] == ',')
+            count++;
+    }
+    return count;
+}
+
+
+void print_file(FILE *p_file, int file_len) {
+    char buffer[file_len];
+    fread(buffer, sizeof(char), 30, p_file);
+    printf("%s", buffer);
+    int lines_num = line_counter(buffer, file_len);
+
+    for (int i = 0; i < lines_num; i++) {
+
+    }
+
+}
 
 
 int main() {
-    FILE *file;
-    file = fopen("default.txt", "r");
-    if (file == NULL) {
+    FILE *p_file = fopen("../input.txt", "r");
+    if (p_file == NULL) {
         puts("Error opening file");
         exit(EXIT_FAILURE);
     }
 
+    fseek(p_file, 0, SEEK_END);
+    int file_len = ftell(p_file);
+    fseek(p_file, 0, SEEK_SET);
+
+
+
+    fclose(p_file);
 }
